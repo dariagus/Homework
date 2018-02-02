31d8a625f1a663101423b851477fc3f72b766de6
Daria Guseva <dis379@icloud.com>
2018-02-02 18:05:22 +0300

    fixed tasks

--- task_02_01.py
+++ task_02_01.py
@@ -1,8 +1,8 @@
 def is_palindrome(x):
-    x = str(x.lower())
+    x = str(x)
+    x = x.lower()
     x = x.replace(' ', '')
     if x == x[::-1]:
         return True
     else:
         return False
-
